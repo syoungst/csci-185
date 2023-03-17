7b501fcd56572fc25e707d2196cf97f2ba030635
Samaya <syoungst@unca.edu>
2023-03-17 10:38:35 -0400

    quiz 2

---
+++ lectures/lecture15/01-style-property-demo/main.js
@@ -0,0 +1,46 @@
+
+function makeRed() {
+    // your code here...
+    console.log('Change background to red');
+    document.querySelector('#section1').style.backgroundColor = 'red';
+    document.querySelector('#section1').style.color = 'white';
+    document.querySelector('#section1').style.fontSize = '20px';
+
+};
+
+function makeBlue() {
+    // your code here...
+    console.log('Change background to blue');
+    document.querySelector('#section2').style.backgroundColor = 'blue';
+    document.querySelector('#section2').style.fontSize = '20px';
+
+};
+
+function makePink() {
+    // your code here...
+    console.log('Change background to pink');
+    document.querySelector('#section3').style.backgroundColor = 'hotpink';
+    document.querySelector('#section3').style.fontSize = '20px';
+
+};
+
+function makeOrange() {
+    // your code here...
+    console.log('Change background to orange');
+    document.querySelector('#section4').style.backgroundColor = 'darkorange';
+    document.querySelector('#section4').style.fontSize = '20px';
+
+};
+
+function multicolor() {
+    // your code here...
+    console.log('Change background to orange');
+    document.querySelector('#section1').style.backgroundColor = 'red';
+    document.querySelector('#section2').style.backgroundColor = 'blue';
+    document.querySelector('#section3').style.backgroundColor = 'hotpink';
+    document.querySelector('#section4').style.backgroundColor = 'darkorange';
+    document.querySelector('#section4').style.backgroundColor = 'darkorange';
+    document.querySelector('#section4').innerHTML = 'YO';
+
+};
+
---
+++ lectures/lecture16/01-warmup/index.js
@@ -0,0 +1,30 @@
+function toggleFall() {
+    /*
+        Your task: target the body element and change it's class to "fall"
+    */
+   document.querySelector('body').className = 'fall'
+}
+
+function toggleWinter() {
+    /*
+        Your task: target the body element and change it's class to "winter"
+    */
+        document.querySelector('body').className = 'winter'
+
+}
+
+function toggleSpring() {
+    /*
+        Your task: target the body element and change it's class to "spring"
+    */
+        document.querySelector('body').className = 'spring'
+
+}
+
+function toggleUNCA() {
+    /*
+        Your task: target the body element and change it's class to "spring"
+    */
+        document.querySelector('body').className = 'unca'
+
+}
